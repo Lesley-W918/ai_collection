19f63114dbd512202541adb1551a23d82d858c8d
Lesley-W918 <wangdan4288@gmail.com>
2026-03-11 18:53:07 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
+import Admin from './Admin.tsx';
 import './index.css';
+
+// Simple client-side routing
+const isAdmin = window.location.pathname === '/admin';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    {isAdmin ? <Admin /> : <App />}
   </StrictMode>,
 );
